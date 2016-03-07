9d07950827d7fbfe4562b5dfceafd07cd23f88af
Guillaume Riou <guiloua2@hotmail.com>
2016-03-06 23:10:23 -0500

    focussable td, no more arrow scroll, space inverts directions

--- croise.js
+++ croise.js
@@ -2,12 +2,53 @@
 var last_case = null;
 var direction = [1, 0];
 $(function(){
+    $(window).keypress(function(event) {
+        // space and arrow keys
+        if($.inArray(event.keyCode, [32, 37, 38, 39, 40])) {
+            event.preventDefault();
+            console.log("prev");
+        }
+    });
 
     $.getJSON("grille_ex.json", function(data){
         gen_grille(data);
+        bind_events();
     });
 
-    alert($(".case")[0]);
+});
+var change_selection = function(new_select){
+    if (!new_select[0]) {
+        return;
+    }
+    if (new_select.hasClass("noire")){
+            return;
+        }
+        $(last_case).removeClass("selected");
+        new_select.addClass("selected");
+        new_select.focus();
+        last_case = new_select;
+}
+
+var gen_grille = function(data){
+    data["diagram"].forEach(function(row, j){
+        $("#grille").find("tbody").append('<tr id="row' + j + '"></tr>');
+        row.split("").forEach(function(charact, i){
+            var cell_html = '<td tabindex="0" id="' + i + "-" + j + '" class="case';
+            cell_html += (charact == ".") ? " noire\"" : "\"";
+            cell_html += (data["numbers"][j][i] != 0) ? " num=\"" + data["numbers"][j][i] + "\" " : "";
+            cell_html += ' sol="'+data["solution"][j][i] + '" ';
+            cell_html += "></td>";
+            $("#row" + j).append(cell_html);
+        });
+    });
+}
+
+var invert_direction = function(){
+    direction[0] = +!direction[0];
+    direction[1] = +!direction[1];
+}
+
+var bind_events = function(){
     $(".case").click(function(){
         if ($(this).hasClass("selected")) {
             invert_direction();
@@ -15,8 +56,9 @@ $(function(){
             change_selection($(this));
         }
     });
-    $(window).keypress(function(event){
+    $(".case").keypress(function(event){
         var current_selection_id = $(".selected").attr("id").split("-");
+        event = event || window.event;
         if(event.keyCode >= 37 && event.keyCode <=40){
             switch(event.keyCode){
                 //left
@@ -42,9 +84,7 @@ $(function(){
                     change_selection(
                         $("#" + current_selection_id[0] +
                          "-" + (+(current_selection_id[1]) + 1)));
-                default:
-                    console.log(event.keyCode);
-                break;
+                    break;
             }
         } else {
             var clef = String.fromCharCode(event.which).toUpperCase();
@@ -52,6 +92,9 @@ $(function(){
                 $(".selected").text($(".selected").attr("sol"));
                 $(".selected").addClass("cheater");
                 $(".selected").removeClass("wrong");
+            } else if(clef == " ") {
+                invert_direction();
+                return;
             } else if(/[a-z]/i.test(clef)){
                 if(clef != $(".selected").attr("sol")){
                     $(".selected").addClass("wrong");
@@ -65,33 +108,4 @@ $(function(){
                          "-" + (+(current_selection_id[1]) + direction[1])));
         }
     });
-});
-var change_selection = function(new_select){
-    if (!new_select[0]) {
-        return;
-    }
-    if (new_select.hasClass("noire")){
-            return;
-        }
-        $(last_case).removeClass("selected");
-        new_select.addClass("selected");
-        last_case = new_select;
-}
-var gen_grille = function(data){
-    data["diagram"].forEach(function(row, j){
-        $("#grille").find("tbody").append('<tr id="row' + j + '"></tr>');
-        row.split("").forEach(function(charact, i){
-            var cell_html = '<td id="' + i + "-" + j + '" class="case';
-            cell_html += (charact == ".") ? " noire\"" : "\"";
-            cell_html += (data["numbers"][j][i] != 0) ? " num=\"" + data["numbers"][j][i] + "\" " : "";
-            cell_html += ' sol="'+data["solution"][j][i] + '" ';
-            cell_html += "></td>";
-            $("#row" + j).append(cell_html);
-        });
-    });
-    return;
-}
-var invert_direction = function(){
-    direction[0] = +!direction[0];
-    direction[1] = +!direction[1];
 }
